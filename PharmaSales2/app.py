e42ae6fd4dfeef6520b55d18ecd7f627d6229f4c
Larry Jokhan <ljokhan@gmail.com>
2026-05-09 22:11:39 -0400

    Added PharmaSales application

---
+++ PharmaSales2/app.py
@@ -0,0 +1,54 @@
+# Import libraries:
+from dash import Dash, html, dcc
+import dash
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__, use_pages=True)
+
+# Configure HTML page:
+app.layout = html.Div(
+    
+    style={
+        'background-image': 'url("/assets/Wallpaper - Pharmacy generic.jpg")',
+        'background-size': 'cover',        # Scales image to cover the entire container
+        'background-repeat': 'no-repeat',  # Prevents the image from tiling
+        'background-position': 'center',   # Centers the image
+        'height': '97vh',                 # Sets height to 100% of the viewport height
+        'width': '97vw'                   # Sets width to 100% of the viewport width
+    },  
+    
+    children=[
+
+    # Add blank line:
+    html.Br(),
+
+    # Add logo:
+    html.Div(children=[
+        html.Img(src='/assets/logo.png', style={'width': '150px'})
+    ],style = {"display":"flex", "justifyContent":"center"}),
+
+    # Add menu title:
+    html.H1('Pharmacy Sales Report Menu', style = {"text-align":"center", "color":"black", 'background-color':'white'}),
+
+    # Create menu as a horizontal table of buttons:
+    html.Table([
+        html.Tr([
+            html.Td([
+                dcc.Link("Daily Sales", href="/PharmaSales-SalesByDay", style = {'color':'black', 'font-size':24, 'text-align':'center', 'font-weight':'bold'})
+            ], style = {'text-align':'center'}),
+            html.Td([
+                dcc.Link("Monthly Sales", href="/PharmaSales-SalesByMonth", style = {'color':'black', 'font-size':24, 'text-align':'center', 'font-weight':'bold'})
+            ], style = {'text-align':'center'}),
+            html.Td([
+                dcc.Link("Yearly Sales", href="/PharmaSales-SalesByYear", style = {'color':'black', 'font-size':24, 'text-align':'center', 'font-weight':'bold'})
+            ], style = {'text-align':'center'}),
+        ])
+    ], style={'width':'100%'}),
+
+    # Page content is loaded here:
+    dash.page_container
+])  
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ PharmaSales2/pages/PharmaSales-SalesByMonth.py
@@ -0,0 +1,13 @@
+import dash
+import pandas as pd
+import urllib
+import plotly.express as px
+from dash import html
+from dash import Dash, dcc, html
+
+dash.register_page(__name__, path='/PharmaSales-SalesByMonth')
+
+layout = html.Div([
+    html.Br(),
+    html.Br()
+])
