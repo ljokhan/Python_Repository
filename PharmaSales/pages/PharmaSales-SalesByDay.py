6d4c3bf385ff0a7261fd890216f8c49a2d47f7f0
Larry Jokhan <ljokhan@gmail.com>
2026-05-16 13:42:14 -0400

    Add timeout to SQL Database connection

--- PharmaSales/pages/PharmaSales-SalesByDay.py
+++ PharmaSales/pages/PharmaSales-SalesByDay.py
@@ -18,39 +18,40 @@ username = 'ljokhan'
 password = 'NissanAltima2013#'
 
 # Build the connection string for SQL Authentication:
-conn_str = (
+connection_string = (
     f"Server={server},1433;"
     f"Database={database};"
     f"UID={username};"
     f"PWD={password};"
     "Encrypt=yes;"
     "TrustServerCertificate=no;"
-    ###"login_timeout=30"
 )
 
 # Establish connection
-with connect(conn_str) as conn:
+#with connect(conn_str) as conn:
     
-    # This is the query used to generate the chart:
-    query = '''SELECT CAST([ReportTimestamp] as date) as ReportDate, 
+# This is the query used to generate the chart:
+query = '''SELECT CAST([ReportTimestamp] as date) as ReportDate, 
             sum([AceticAcidDerivatives] + [PropionicAcidDerivatives] + [SalicylicAcidDerivatives] + [PyrazolonesAndAnilides] + [AnxiolyticDrugs]
                 + [HypnoticsSndSedativesDrugs] + [ObstructiveAirwayDrugs] + [Antihistamines] ) as TotalSales
             FROM dbo.PharmaDrugSalesbyHour
             WHERE [ReportTimestamp] >= '2019-10-01'
             GROUP BY CAST([ReportTimestamp] as date) '''
 
-    # Create data frame using query above:
-    df = pd.read_sql_query(query, conn)  
-    df.sort_values(by='ReportDate')
+conn = connect(connection_string, timeout=30)
+
+# Create data frame using query above:
+df = pd.read_sql_query(query, conn)  
+df.sort_values(by='ReportDate')
     
-    # Create line chart using the dataframe:
-    fig = px.line(df, x="ReportDate", y="TotalSales", 
+# Create line chart using the dataframe:
+fig = px.line(df, x="ReportDate", y="TotalSales", 
               title='<b> Daily Pharmaceutical Drug Sales - All Product Lines',
               markers=True)
     
-    # Format the line chart:
-    fig.update_traces(line_color="#c74d06")
-    fig.update_layout(title_x=0.5) # Center title
+# Format the line chart:
+fig.update_traces(line_color="#c74d06")
+fig.update_layout(title_x=0.5) # Center title
 
 # Display page:
 layout = html.Div([
