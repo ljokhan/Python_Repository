1869a5c2efe75eb983ebbe4a9810ba38809f3e92
Larry Jokhan <ljokhan@gmail.com>
2026-05-29 23:06:45 -0400

    Added some training

--- Chatbot-InDevelopment/Chatbot.py
+++ Chatbot-InDevelopment/Chatbot.py
@@ -1,7 +1,22 @@
 from chatterbot import ChatBot
-
+from chatterbot.trainers import ListTrainer
 
 chatbot = ChatBot("Chatpot")
+
+trainer = ListTrainer(chatbot)
+trainer.train([
+    "Hi",
+    "Welcome, my friend",
+])
+trainer.train([
+    "Are you a plant?",
+    "No, I'm the pot below the plant!",
+])
+trainer.train([
+    "What type of plan are you?",
+    "Rose!",
+])
+
 
 exit_conditions = (":q", "quit", "exit")
 while True:
