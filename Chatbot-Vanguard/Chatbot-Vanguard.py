41c4d624bd9586414f7916e1135a489f9eb7677b
Larry Jokhan <ljokhan@gmail.com>
2026-06-01 11:35:10 -0400

    Added connection to Azure SQL Database

--- Chatbot-Vanguard/Chatbot-Vanguard.py
+++ Chatbot-Vanguard/Chatbot-Vanguard.py
@@ -1,10 +1,43 @@
 # Import libraries:
 from chatterbot import ChatBot
 from chatterbot.trainers import CsvFileTrainer
+from mssql_python import connect
+import pandas as pd
+
+# Connect to Azure SQL database, to train the bot:
+server = 'sql-db-01-ljokhan-server.database.windows.net'
+database = 'sql-db-01-ljokhan'
+username = 'PythonUser'
+password = 'ILovePython2026!'
+
+# For development, use SQL Authentication. Build the connection string for SQL Authentication:
+connection_string = (
+    f"Server={server},1433;"
+    f"Database={database};"
+    f"UID={username};"
+    f"PWD={password};"
+    "Encrypt=yes;"
+    "TrustServerCertificate=no;"
+)
+
+# This is the query used to generate the dataframe:
+query = 'SELECT question, answer FROM [chatbot].[chatbot-Vanguard] '
+
+# Establish connection with a timeout:
+conn = connect(connection_string, timeout=180)
+
+# Create data frame using query above:
+df = pd.read_sql_query(query, conn)  
+
+for index, row in df.iterrows():
+    print (row['question'])
+    print (row['answer'])
 
 # Initialize the chatbot instance:
 bot_Vanguard = ChatBot('DataBot')
 
+
+'''
 # Bind the CSV File Trainer to your chatbot:
 trainer = CsvFileTrainer(
     bot_Vanguard,
@@ -36,3 +69,4 @@ while True:
         
     except (KeyboardInterrupt, EOFError, SystemExit):
         break
+'''
